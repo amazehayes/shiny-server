c83914d049adf1ba4c451b8fffb68a58035feb16
amazehayes <30516708+amazehayes@users.noreply.github.com>
2018-02-20 16:27:38 -0500

    Add files via upload

--- app.R
+++ app.R
@@ -340,7 +340,7 @@ ui <- dashboardPage(
                               width:100%;
                               }
                               .content {
-                              padding-top: 60px;
+                              padding-top: 80px;
                               }'))),
     tabItems(
     tabItem(tabName = "welcome",
